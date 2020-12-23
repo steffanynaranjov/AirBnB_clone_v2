82abbb97c6c67fe720a9b636f66228cbb9ed4505
steffanynaranjov <steffany.naranjo@gmail.com>
2020-12-23 01:30:39 +0000

    trying again

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 """Script that distributes an archive to your web servers"""
-from fabric.api import run, put, env
-from datetime import datetime
-from os import path
-
+from fabric.operations import put, run, sudo
+from fabric.api import *
+import os.path
+import time
 
 env.hosts = ['35.227.50.149', '34.73.221.231']
 env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Deploy"""
-    if not path.exists(archive_path):
+    """
+        Deploy server
+    """
+    if not os.path.isfile(archive_path):
         return False
+
     try:
-        file = archive_path.split("/")[-1]
-        dir = "/data/web_static/releases/" + file.split(".")[0]
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(dir))
-        run("sudo tar -xzf /tmp/{} -C {}".format(file, dir))
-        run("sudo rm /tmp/{}".format(file))
-        run("sudo mv {}/web_static/* {}/".format(dir, dir))
-        run("sudo rm -rf {}/web_static".format(dir))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf {} /data/web_static/current".format(dir))
-        print("New version deployed!")
+        file_ext = archive_path.split("/")[-1]
+        file = '/data/web_static/releases/{}'.format(file_ext.split(".")[0])
+        put(archive_path, "/tmp")
+
+        run("mkdir -p {}".format(file))
+
+        run("tar -xzf /tmp/{} -C {}/".format(file_ext, file))
+        run("rm /tmp/{}".format(file_ext))
+
+        run("mv {0}/web_static/* {0}".format(file))
+        run("rm -rf {}/web_static".format(file))
+        run("rm -rf /data/web_static/current")
+
+        run("ln -s {} /data/web_static/current".format(file))
         return True
     except:
         return False
