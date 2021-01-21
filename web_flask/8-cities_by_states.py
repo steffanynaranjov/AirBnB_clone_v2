7756e1f76fe4f5a7e409f5bf44a2abeb33ec19a0
steffanynaranjov <steffany.naranjo@gmail.com>
2021-01-21 01:11:05 +0000

    cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(self):
+    """ clse the session """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def cities_list():
+    """Display States"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
