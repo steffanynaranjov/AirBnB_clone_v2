e13e09515f0589f4528eb5600a6c84d26084d7c5
steffanynaranjov <steffany.naranjo@gmail.com>
2021-01-21 01:03:24 +0000

    seven ag

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,17 +6,17 @@ from models import storage
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def close(self):
+    """ clse the session """
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display States"""
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown_db(self):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
 
 
 if __name__ == '__main__':
