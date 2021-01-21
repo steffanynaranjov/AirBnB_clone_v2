099fcab987c053c897bc655f5d4f176ff48ab850
steffanynaranjov <steffany.naranjo@gmail.com>
2021-01-21 00:44:50 +0000

    uptd

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
 
-from flask import render_template
+from flask import Flask, render_template
 from models import storage
 app = Flask(__name__)
 
@@ -19,5 +19,5 @@ def teardown_db(self):
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
